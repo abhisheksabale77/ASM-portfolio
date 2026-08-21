3d078a6de413fbc8dfdf8e55b338ee9fa2c38fda
abhisheksabale777 <abhishekbsabale8629@gmail.com>
2026-08-21 13:29:02 +0530

    Update hero section according to any devices and configuration

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
       {/* Background Wrapper */}
       <div className="absolute inset-0">
         <div
-          className="w-full h-full bg-cover bg-[65%_center] sm:bg-[center_right] md:bg-[65%_center] bg-no-repeat"
+          className="w-full h-full bg-cover bg-[67%_center] sm:bg-[center_right] md:bg-[65%_center] bg-no-repeat"
           style={{ backgroundImage: "url('/backgroundImage.png')" }}
         />
       </div>
